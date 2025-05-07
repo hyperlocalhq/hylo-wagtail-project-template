3b4b07f7810680f549269d2017dcfa6e18924421
Aidas Bendoraitis <bendoraitis@studio38.de>
2025-05-07 18:16:36 +0200

    🔨 [HYP-119] The project refactored by moving the apps under apps package. AI-ignore files added.

--- project_name/apps/images/apps.py
+++ project_name/apps/images/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ImagesConfig(AppConfig):
     default_auto_field: str = "django.db.models.AutoField"
-    name = "{{ project_name }}.images"
+    name = "{{ project_name }}.apps.images"
